3b329652d882c6082516b0763a5091f714b47cb5
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-25 15:42:02 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -14,4 +14,11 @@ class BankAccount
   def display_balance
       "Your balance is $#{balance}."
   end
+  def valid?
+    if status == "open"  && balance > 0
+      true
+    else
+      false
+    end
+  end
 end
