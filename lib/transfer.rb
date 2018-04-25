5d92c5aaddc741118e8daec74c078d99fec497f4
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-25 16:16:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,10 +3,10 @@ class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
 
-  def initialize(sender)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
+    @amount = amount
     @status = "pending"
-    @transfer = transfer
   end
 end
