91d4c616baf319a8bcd3596ac53b5f1bb09ac8c2
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-25 16:58:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,10 +14,10 @@ class Transfer
     sender.valid? && receiver.valid?
   end
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "complete"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
-      self.status == "complete"
+      self.status = "complete"
     else
       reject_transfer
     end
